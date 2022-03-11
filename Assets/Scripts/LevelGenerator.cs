d359f077075afcea5511e3315221cf9136d81480
Pavel Sarmiento <pavelsarmiento@hotmail.com>
2022-03-12 00:16:10 +0100

    Add: Navmesh player walk and new navmesh component

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LevelGenerator : MonoBehaviour{
+    
+    
+    [SerializeField] GameObject generateWall;
+    [SerializeField] GameObject player;
+
+    public NavMeshSurface surface;
+    
+    bool playerSpawned = false;
+
+    void Start(){
+        
+        //Generate level
+        
+        // Update NavmMesh
+        surface.BuildNavMesh();
+    }
+}
